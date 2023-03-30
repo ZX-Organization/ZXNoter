4fe5b15802c6429dfaac04ad65903955ed56200e
zedoCN <zedoCN@outlook.com>
2023-03-30 18:11:46 +0800

    这是测试

--- src/com/github/zxorganization/main/Main.java
+++ src/com/github/zxorganization/main/Main.java
@@ -1,5 +1,10 @@
 package com.github.zxorganization.main;
 
 public class Main {
-
+    void aaa(){
+        //zedo的函数
+    }
+    public static void main(String[] args) {
+        System.out.println("草泥马");
+    }
 }
