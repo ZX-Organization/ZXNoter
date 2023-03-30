5025b96a6fadcceaca59ad73ad08321a9d2e493e
zedoCN <zedoCN@outlook.com>
2023-03-30 21:07:24 +0800

    重新刷新构建项目

---
+++ src/main/java/com/github/zxorganization/io/Main.java
@@ -0,0 +1,11 @@
+package com.github.zxorganization.io;
+
+import com.github.zxorganization.ui.ZXNoterApp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Main.class.getResource(""));
+        ZXNoterApp zxNoterApp = new ZXNoterApp();
+        zxNoterApp.runApp(args);
+    }
+}
