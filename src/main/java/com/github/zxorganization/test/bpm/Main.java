74f14110f8a80b52173525a2c62b448d0132583c
zedoCN <zedoCN@outlook.com>
2023-06-08 00:51:30 +0800

    变更

---
+++ src/main/java/com/github/zxorganization/test/bpm/Main.java
@@ -0,0 +1,10 @@
+package com.github.zxorganization.test.bpm;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class);
+
+    }
+}
