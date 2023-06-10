ac8a2d1a6822a57ffe6fd35eb64220b02a2ec03c
xiangCaO233 <odokoei@outlook.com>
2023-06-11 01:39:26 +0800

    test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.alibaba.fastjson2.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        JSONObject a = new JSONObject();
+        a.put("1" , "a");
+        System.out.println(a);
+    }
+}
