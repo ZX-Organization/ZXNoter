74f8b2f55ad0b49e3b401e265f284e29efbb4af3
zedoCN <zedoCN@outlook.com>
2023-06-12 01:10:52 +0800

    update

--- src/team/zxorg/zxnoter/ui/ZXNApp.java
+++ src/team/zxorg/zxnoter/ui/ZXNApp.java
@@ -1,6 +1,9 @@
 package team.zxorg.zxnoter.ui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ZXNApp extends Application {
@@ -8,6 +11,22 @@ public class ZXNApp extends Application {
     public void start(Stage stage) {
 
 
+        VBox titlePane = new VBox();
+        titlePane.setBackground(Background.fill(Color.RED));
+        titlePane.setPrefSize(Region.USE_COMPUTED_SIZE, 30);
+
+        HBox bodyPane = new HBox();
+        bodyPane.setBackground(Background.fill(Color.GREEN));
+        bodyPane.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+        VBox.setVgrow(bodyPane, Priority.ALWAYS);
+
+        VBox rootPane = new VBox(titlePane, bodyPane);
+        rootPane.setBackground(Background.fill(Color.YELLOW));
+
+        Scene mainScene = new Scene(rootPane);
+
+
+        stage.setScene(mainScene);
         stage.setMinWidth(800);
         stage.setMinHeight(600);
         stage.setWidth(800);
