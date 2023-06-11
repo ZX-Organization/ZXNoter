5fe3f02af0841d79c668f7522fcf1cb1c5b4fdf0
zedoCN <zedoCN@outlook.com>
2023-06-12 01:01:44 +0800

    update

--- src/team/zxorg/zxnoter/Main.java
+++ src/team/zxorg/zxnoter/Main.java
@@ -1,7 +1,14 @@
 package team.zxorg.zxnoter;
 
+import team.zxorg.zxnoter.ui.ZXNApp;
+
 public class Main {
     public static void main(String[] args) {
+
+        /**
+         * 测试UI
+         */
+        ZXNApp.run();
 
     }
 }
