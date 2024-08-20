e77c0b57ac73968ceb01a6f8c3720d47f11340d7
zedoCN <zedoCN@outlook.com>
2024-08-21 01:46:39 +0800

    欸牢翔

--- new/ExtensionApi/src/team/zxorg/zxnoter/api/extension/ExtensionEvent.java
+++ new/ExtensionApi/src/team/zxorg/zxnoter/api/extension/ExtensionEvent.java
@@ -1,5 +1,4 @@
 package team.zxorg.zxnoter.api.extension;
 
 public class ExtensionEvent {
-    public static final EventType<ExtensionEvent> MOUSE_CLICKED = new EventType<>(MouseEvent.ANY, "MOUSE_CLICKED");
 }
