3feeb4bc01400beeb30b92ba2cd66bf39ad967f9
zedoCN <zedoCN@outlook.com>
2024-12-03 21:52:57 +0800

    改改名字，短点好，累了

--- zxnoter-core-utils/src/main/kotlin/org/zxnoter/utils/Node.kt
+++ zxnoter-core-utils/src/main/kotlin/org/zxnoter/utils/Node.kt
@@ -3,16 +3,15 @@ package org.zxnoter.utils
 import org.zxnoter.api.node.TreeNode
 import org.zxnoter.core.TreeNodeImpl
 
-object Node {
-    fun <T> createTreeNode(): TreeNode<T> {
-        return TreeNodeImpl()
-    }
 
-    fun <T> createTreeNode(value: T? = null): TreeNode<T> {
-        return TreeNodeImpl(value)
-    }
+fun <T> treeNode(): TreeNode<T> {
+    return TreeNodeImpl()
+}
 
-    fun <T> createTreeNode(name: String, value: T? = null): TreeNode<T> {
-        return TreeNodeImpl(name, value)
-    }
+fun <T> treeNode(value: T? = null): TreeNode<T> {
+    return TreeNodeImpl(value)
+}
+
+fun <T> treeNode(name: String, value: T? = null): TreeNode<T> {
+    return TreeNodeImpl(name, value)
 }
--- zxnoter-core-utils/src/test/kotlin/TreeNodeTest.kt
+++ zxnoter-core-utils/src/test/kotlin/TreeNodeTest.kt
@@ -1,9 +1,9 @@
-import org.zxnoter.utils.Node
+import org.zxnoter.utils.treeNode
 import java.nio.file.*
 import java.nio.file.attribute.BasicFileAttributes
 
 fun main() {
-    val node = Node.createTreeNode<Path>()
+    val node = treeNode<Path>()
     Files.walkFileTree(Paths.get("./"), object : SimpleFileVisitor<Path>() {
         override fun visitFile(file: Path?, attrs: BasicFileAttributes?): FileVisitResult {
             var p = file.toString().substring(2).split("\\")
@@ -11,6 +11,5 @@ fun main() {
             return FileVisitResult.CONTINUE
         }
     })
-
     println(node.treeToString(StringBuilder()))
 }
