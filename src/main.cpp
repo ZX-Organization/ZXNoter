1e29eb81f6e16fec89be5aee4574208ed8382328
xiangCaO233 <odokoei@outlook.com>
2024-12-26 21:09:44 +0800

    更改为mainwindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
-#include "qt/ui/mainwidget.h"
+#include "qt/ui/mainwindow.h"
 
 #include <iostream>
-#include <iterator>
 #include <ostream>
 #include <qapplication.h>
-#include <random>
-#include <string>
-#include <unordered_map>
-#include <vector>
 
 int main(int argc, char *argv[]) {
   std::cout << "nmsl " << std::endl;
   QApplication app(argc, argv);
 
   // 测试main窗口
-  mainwidget w;
+  MainWindow w;
   w.show();
-
-  std::random_device dev;
-  std::uniform_int_distribution<> dist(0, 20000); //[v1,v2)
-  std::vector<std::string> events = {"zxnoter😰", "精灵幻想记!😊", "hyp🤬"};
-  std::unordered_map<int, int> indicies = {{0,0},{1,0},{2,0}};
-  for (int i = 0; i < 100000000; i++)
-    indicies[dist(dev) %3] ++;
-  for (auto& pair: indicies) 
-    std::cout <<"value:["+events[pair.first]+"]->"<< "counts:["+std::to_string(pair.second)+"]"<< std::endl;
 
   // 进入qt事件循环
   return app.exec();
