e353a9a7345e50ae2b3ce7a6c57bd1dbef94df96
xiangCaO233 <odokoei@outlook.com>
2024-12-26 17:32:05 +0800

    谁寄

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "qt/ui/mainwidget.h"
 
 #include <iostream>
+#include <iterator>
+#include <ostream>
 #include <qapplication.h>
+#include <random>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 int main(int argc, char *argv[]) {
   std::cout << "nmsl " << std::endl;
@@ -10,6 +16,15 @@ int main(int argc, char *argv[]) {
   // 测试main窗口
   mainwidget w;
   w.show();
+
+  std::random_device dev;
+  std::uniform_int_distribution<> dist(0, 20000); //[v1,v2)
+  std::vector<std::string> events = {"zxnoter😰", "精灵幻想记!😊", "hyp🤬"};
+  std::unordered_map<int, int> indicies = {{0,0},{1,0},{2,0}};
+  for (int i = 0; i < 100000000; i++)
+    indicies[dist(dev) %3] ++;
+  for (auto& pair: indicies) 
+    std::cout <<"value:["+events[pair.first]+"]->"<< "counts:["+std::to_string(pair.second)+"]"<< std::endl;
 
   // 进入qt事件循环
   return app.exec();
