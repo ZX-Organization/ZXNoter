381a3cb57a3305d78f31b73eff2ae4377517f618
2333xiang <odokoei@outlook.com>
2024-12-27 15:42:23 +0800

    创建顶点缓冲对象

--- src/qt/gl/glcanvas.h
+++ src/qt/gl/glcanvas.h
@@ -7,19 +7,21 @@
 #include <qopenglwidget.h>
 
 class GLCanvas : public QOpenGLWidget, QOpenGLFunctions {
-  Q_OBJECT
-public:
-  // 构造&析构
-  GLCanvas(QWidget *parent = nullptr);
-  virtual ~GLCanvas();
+    Q_OBJECT
+   public:
+    // 构造&析构
+    GLCanvas(QWidget *parent = nullptr);
+    virtual ~GLCanvas();
 
-protected:
-  // 初始化gl环境
-  void initializeGL() override;
-  // 渲染内容
-  void paintGL() override;
-  // 调整大小
-  void resizeGL(int w, int h) override;
+   protected:
+    // 顶点数组对象,顶点缓冲对象,帧缓冲对象,Uniform缓冲对象
+    uint32_t VAO, VBO, FBO, UBO;
+    // 初始化gl环境
+    void initializeGL() override;
+    // 渲染内容
+    void paintGL() override;
+    // 调整大小
+    void resizeGL(int w, int h) override;
 };
 
-#endif // ZXNOTER_GLCANVAS_H
+#endif  // ZXNOTER_GLCANVAS_H
