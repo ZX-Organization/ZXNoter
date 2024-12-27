ab2af68cc2bbc474389a8b598efc87ab24b0209a
2333xiang <odokoei@outlook.com>
2024-12-27 17:29:47 +0800

    格式化代码

--- src/qt/ui/mainwindow.h
+++ src/qt/ui/mainwindow.h
@@ -7,16 +7,15 @@ namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+ public:
+  explicit MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+ private:
+  Ui::MainWindow *ui;
 };
 
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
